47093fb49e4769b36b48089c3bef4286cc91e8f1
Splleat <Splleat@gmail.com>
2026-02-09 10:14:17 +0900

    feat: step2 테스트 완료

--- src/test/java/com/nhnacademy/http/SimpleHttpServerTest.java
+++ src/test/java/com/nhnacademy/http/SimpleHttpServerTest.java
@@ -24,6 +24,9 @@ import java.net.http.HttpResponse;
 import java.util.Objects;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @Slf4j
 class SimpleHttpServerTest {
     static SimpleHttpServer simpleHttpServer;
@@ -58,7 +61,7 @@ class SimpleHttpServerTest {
         log.debug("response:{}",response.body());
 
         //TODO#100 - response.statusCode() == 200 검증 합니다.
-
+        assertEquals(200, response.statusCode());
     }
 
     @Test
@@ -73,7 +76,8 @@ class SimpleHttpServerTest {
 
         //TODO#101 - response.body() 'hello' or 'java' 문자열이 포함되었는지 검증 합니다.
         Assertions.assertAll(
-
+                () -> assertTrue(response.body().contains("hello")),
+                () -> assertTrue(response.body().contains("java"))
         );
     }
 
@@ -91,16 +95,23 @@ class SimpleHttpServerTest {
         log.debug("contentType:{}",actual);
 
         //TODO#102 contentType이 'text/html' 검증 합니다.
-
-
+        assertTrue(actual.contains("text/html"));
     }
 
     @Test
     @DisplayName("charset utf-8")
     void request4() throws URISyntaxException, IOException, InterruptedException {
         //TODO#103 contentType header의 charset=utf-8 인지 검증 합니다.
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(String.format("http://localhost:%d",TEST_PORT)))
+                .build();
 
+        HttpResponse<String> response = httpClient.send(request,HttpResponse.BodyHandlers.ofString());
+        Optional<String> contentTypeOptional = response.headers().firstValue("Content-Type");
+        String actual = contentTypeOptional.get().toLowerCase();
 
+        assertTrue(actual.contains("utf-8"));
     }
 
 
@@ -108,12 +119,19 @@ class SimpleHttpServerTest {
     @DisplayName("Content-Length")
     void request5() throws URISyntaxException, IOException, InterruptedException {
         //TODO#104 content-Length 값이 존재하는지 검증 합니다.
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(String.format("http://localhost:%d",TEST_PORT)))
+                .build();
 
+        HttpResponse<String> response = httpClient.send(request,HttpResponse.BodyHandlers.ofString());
+        Optional<String> contentLengthOptional = response.headers().firstValue("Content-Length");
+
+        assertTrue(contentLengthOptional.isPresent());
     }
 
     @AfterAll
     static void tearDown() throws InterruptedException {
         Thread.sleep(1000);
     }
-
 }
